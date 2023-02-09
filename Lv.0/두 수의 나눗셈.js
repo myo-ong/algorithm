2ffe8db633bd989200c5f7c78852c58051a3b215
myo-ong <myo.ongzzz@gmail.com>
2023-02-09 09:54:09 +0900

    두 수의 나눗셈 프로그래머스

---
+++ Lv.0/두 수의 나눗셈.js
@@ -0,0 +1,11 @@
+function solution(num1, num2) {
+  return parseInt((num1 / num2) * 1000);
+}
+
+function solution(num1, num2) {
+  return Math.trunc((num1 / num2) * 1000);
+}
+
+function solution(num1, num2) {
+  return ~~((num1 / num2) * 1000);
+}
