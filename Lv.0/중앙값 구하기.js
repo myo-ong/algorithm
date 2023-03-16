f3adc3f5289fea27f123fc04092e24b3b4a95d6f
myo-ong <myo.ongzzz@gmail.com>
2023-03-16 15:48:46 +0900

    나머지 구하기  프로그래머스

--- Lv.0/중앙값 구하기.js
+++ Lv.0/중앙값 구하기.js
@@ -1,3 +1,7 @@
-function solution(num1, num2) {
-  return num1 % num2;
+function solution(array) {
+    return array.sort((acc,cur) => acc-cur)[~~(array.length/2)];
+}
+
+function solution(array) {
+     return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
 }
